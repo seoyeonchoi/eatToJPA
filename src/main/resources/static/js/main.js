c0acd8c8ab2adb18ea54bb50bff5df3f23de5b11
seoyeonchoi <westkite720@gmail.com>
2024-02-15 15:06:18 +0900

    240214: 끝나간다...

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -151,6 +151,94 @@ function populateMembers(boardId) {
     });
 }
 
+// $(document).ready(function() {
+//     // 각 행을 클릭할 때 모달에 해당 게시물의 세부 정보를 표시
+//     $(document).on('click', '#boardsByMeetDate tr', function() {
+//         let boardId = $(this).data('id'); // 각 행의 게시물 ID 가져오기
+//         let userId = document.getElementById('userId').getAttribute('data-userId');
+//
+//         console.log(boardId);
+//         $.ajax({
+//             type: 'GET',
+//             url: '/api/board/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
+//             success: function(data) {
+//                 // 서버로부터 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
+//                 $('#boardDetailModalTitle').text(data.title);
+//                 var statusText = (data.completed == 0) ? '모집중' : '마감';
+//                 var modalStatus = $('#boardDetailModalStatus');
+//                 if (statusText === "모집중") {
+//                     modalStatus.css('background', '#55DDA9');
+//                     modalStatus.css('color', '#000000');
+//                 } else if (statusText === "마감") {
+//                     modalStatus.css('background', '#868296');
+//                     modalStatus.css('color', '#ffffff');
+//                 }
+//                 $('#boardDetailModalStatus').text(statusText);
+//                 $('#boardDetailModalMintoMax').text('최소인원 ~ 최대인원: ' + data.minNum + ' ~ ' + data.maxNum);
+//                 $('#boardDetailModalCurrent').text('현재 신청 인원: ' + data.currentMember);
+//                 $('#boardDetailModalWriter').text('작성자: ' + data.writer);
+//                 $('#boardDetailModalMeetDate').text('식사일자: ' + data.meetDate);
+//                 $('#boardDetailModalContent').text(data.content);
+//                 $('#boardDetailModalResName').text(data.restaurantName);
+//                 $('#boardDetailModalMeetName').text(data.meetName);
+//                 $('#boardDetailModalReskey').text(data.restaurantKey);
+//                 $('#boardDetailModalMeetkey').text(data.meetKey);
+//                 $('#ModalBoardId').text(data.id);
+//
+//                 console.log('userId: ', userId);
+//                 if (data.memberId === userId) {
+//                     // 유저의 memberId와 view가 가진 userId가 같은 경우
+//                     $('#modalBtn1').text('수정하기').show();
+//                     $('#modalBtn2').text('삭제하기').show();
+//                     $('#modalBtn3').text('마감하기');
+//                 } else {
+//                     if (data.completed === 1) {
+//                         $('#modalBtn1').hide();
+//                         $('#modalBtn2').hide();
+//                         $('#modalBtn3').hide();
+//                     } else {
+//                         // 다른 경우
+//                         $('#modalBtn1').hide();
+//                         $('#modalBtn2').hide();
+//                         $('#modalBtn3').text('참석하기');
+//                     }
+//                 }
+//
+//                 populateMembers(boardId);
+//                 getReplyCount(boardId).then(r => console.log(r))
+//                 getReply(boardId).then(r => console.log(r));
+//
+//
+//             },
+//             error: function(xhr, status, error) {
+//                 // 오류 처리
+//                 console.error(xhr.responseText);
+//             }
+//         });
+//
+//         $.ajax({
+//             type: 'GET',
+//             url: '/api/attention/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
+//             success: function(data) {
+//                 console.log('참석취소 불러오는 함수', data)
+//                 // 서버로부터 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
+//                 if (data === true) {
+//                     // 유저가 참석 등록한 경우
+//                     $('#modalBtn1').hide();
+//                     $('#modalBtn2').hide();
+//                     $('#modalBtn3').text('참석취소');
+//                 }
+//
+//             },
+//             error: function(xhr, status, error) {
+//                 // 오류 처리
+//                 console.error(xhr.responseText);
+//             }
+//         });
+//
+//     });
+// });
+
 $(document).ready(function() {
     // 각 행을 클릭할 때 모달에 해당 게시물의 세부 정보를 표시
     $(document).on('click', '#boardsByMeetDate tr', function() {
@@ -158,85 +246,103 @@ $(document).ready(function() {
         let userId = document.getElementById('userId').getAttribute('data-userId');
 
         console.log(boardId);
-        $.ajax({
-            type: 'GET',
-            url: '/api/board/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
-            success: function(data) {
-                // 서버로부터 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
-                $('#boardDetailModalTitle').text(data.title);
-                var statusText = (data.completed == 0) ? '모집중' : '마감';
-                var modalStatus = $('#boardDetailModalStatus');
-                if (statusText === "모집중") {
-                    modalStatus.css('background', '#55DDA9');
-                    modalStatus.css('color', '#000000');
-                } else if (statusText === "마감") {
-                    modalStatus.css('background', '#868296');
-                    modalStatus.css('color', '#ffffff');
-                }
-                $('#boardDetailModalStatus').text(statusText);
-                $('#boardDetailModalMintoMax').text('최소인원 ~ 최대인원: ' + data.minNum + ' ~ ' + data.maxNum);
-                $('#boardDetailModalCurrent').text('현재 신청 인원: ' + data.currentMember);
-                $('#boardDetailModalWriter').text('작성자: ' + data.writer);
-                $('#boardDetailModalMeetDate').text('식사일자: ' + data.meetDate);
-                $('#boardDetailModalContent').text(data.content);
-                $('#boardDetailModalResName').text(data.restaurantName);
-                $('#boardDetailModalMeetName').text(data.meetName);
-                $('#boardDetailModalReskey').text(data.restaurantKey);
-                $('#boardDetailModalMeetkey').text(data.meetKey);
-                $('#ModalBoardId').text(data.id);
 
-                console.log('userId: ', userId);
-                if (data.memberId === userId) {
-                    // 유저의 memberId와 view가 가진 userId가 같은 경우
-                    $('#modalBtn1').text('수정하기').show();
-                    $('#modalBtn2').text('삭제하기').show();
-                    $('#modalBtn3').text('마감하기');
-                } else {
-                    if (data.completed === 1) {
+        // AJAX 요청 통합
+        $.when(
+            $.ajax({
+                type: 'GET',
+                url: '/api/board/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
+                success: function(data) {
+                    // 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
+                    updateModalWithData(data, userId, boardId);
+                },
+                error: function(xhr, status, error) {
+                    // 오류 처리
+                    console.error(xhr.responseText);
+                }
+            }),
+            $.ajax({
+                type: 'GET',
+                url: '/api/attention/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
+                success: function(data) {
+                    console.log('참석취소 불러오는 함수', data)
+                    // 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
+                    if (data == true) {
+                        // 유저가 참석 등록한 경우
                         $('#modalBtn1').hide();
                         $('#modalBtn2').hide();
-                        $('#modalBtn3').hide();
-                    } else {
-                        // 다른 경우
-                        $('#modalBtn1').hide();
-                        $('#modalBtn2').hide();
-                        $('#modalBtn3').text('참석하기');
+                        $('#modalBtn3').text('참석취소');
                     }
+
+                },
+                error: function(xhr, status, error) {
+                    // 오류 처리
+                    console.error(xhr.responseText);
                 }
-
-                populateMembers(boardId);
-                getReplyCount(boardId).then(r => console.log(r))
-                getReply(boardId).then(r => console.log(r));
-
-
-            },
-            error: function(xhr, status, error) {
-                // 오류 처리
-                console.error(xhr.responseText);
+            })
+        ).then(function(response1, response2) {
+            // 두 개의 AJAX 요청이 모두 완료된 후에 실행되는 코드
+            console.log(response1, response2);
+            // 여기에 추가적인 로직을 넣을 수 있습니다.
+            if (response2[0] === true) {
+                // 유저가 참석 등록한 경우
+                $('#modalBtn1').hide();
+                $('#modalBtn2').hide();
+                $('#modalBtn3').text('참석취소');
             }
+
         });
-
-        $.ajax({
-            type: 'GET',
-            url: '/api/attention/' + boardId, // 서버에서 게시물 세부 정보 가져오는 엔드포인트
-            success: function(data) {
-                // 서버로부터 성공적으로 데이터를 가져왔을 때 모달 내부 업데이트
-                if (data === true) {
-                    // 유저가 참석 등록한 경우
-                    $('#modalBtn1').hide();
-                    $('#modalBtn2').hide();
-                    $('#modalBtn3').text('참석취소');
-                }
-
-            },
-            error: function(xhr, status, error) {
-                // 오류 처리
-                console.error(xhr.responseText);
-            }
-        });
-
     });
 });
+
+// 모달 업데이트 함수
+function updateModalWithData(data, userId, boardId) {
+    $('#boardDetailModalTitle').text(data.title);
+    var statusText = (data.completed == 0) ? '모집중' : '마감';
+    var modalStatus = $('#boardDetailModalStatus');
+    if (statusText === "모집중") {
+        modalStatus.css('background', '#55DDA9');
+        modalStatus.css('color', '#000000');
+    } else if (statusText === "마감") {
+        modalStatus.css('background', '#868296');
+        modalStatus.css('color', '#ffffff');
+    }
+    $('#boardDetailModalStatus').text(statusText);
+    $('#boardDetailModalMintoMax').text('최소인원 ~ 최대인원: ' + data.minNum + ' ~ ' + data.maxNum);
+    $('#boardDetailModalCurrent').text('현재 신청 인원: ' + data.currentMember);
+    $('#boardDetailModalWriter').text('작성자: ' + data.writer);
+    $('#boardDetailModalMeetDate').text('식사일자: ' + data.meetDate);
+    $('#boardDetailModalContent').text(data.content);
+    $('#boardDetailModalResName').text(data.restaurantName);
+    $('#boardDetailModalMeetName').text(data.meetName);
+    $('#boardDetailModalReskey').text(data.restaurantKey);
+    $('#boardDetailModalMeetkey').text(data.meetKey);
+    $('#ModalBoardId').text(data.id);
+
+    console.log('userId: ', userId);
+    if (data.memberId === userId) {
+        // 유저의 memberId와 view가 가진 userId가 같은 경우
+        $('#modalBtn1').text('수정하기').show();
+        $('#modalBtn2').text('삭제하기').show();
+        $('#modalBtn3').text('마감하기');
+    } else {
+        if (data.completed === 1) {
+            $('#modalBtn1').hide();
+            $('#modalBtn2').hide();
+            $('#modalBtn3').hide();
+        } else {
+            // 다른 경우
+            $('#modalBtn1').hide();
+            $('#modalBtn2').hide();
+            $('#modalBtn3').text('참석하기');
+        }
+    }
+
+    populateMembers(boardId);
+    getReplyCount(boardId).then(r => console.log(r))
+    getReply(boardId).then(r => console.log(r));
+}
+
 
 // 클릭 이벤트 핸들러
 document.getElementById('new-board-form').addEventListener('click', event => {
@@ -333,7 +439,7 @@ document.getElementById('modalBtn3').addEventListener('click', event => {
     let boardId = document.getElementById('ModalBoardId').innerHTML;
     let btnContent = document.getElementById('modalBtn3').innerHTML;
     if (btnContent === '참석하기') {
-        if (confirm('식사에 참석하시겠습니다?')){
+        if (confirm('식사에 참석하시겠습니까?')){
             fetch(`/api/reservation/${boardId}`, {
                     method: 'GET'
                 }
